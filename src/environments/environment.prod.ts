793dd573a54780bdc12577f03b7c0f759c6c72cf
Kaustav Banerjee <kaustavbanerjee96@gmail.com>
2020-08-13 15:40:42 -0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBrsh71BKWFf24o9r6WHsVcR2_8QQX45oo",
+    authDomain: "todolistapp-202a8.firebaseapp.com",
+    databaseURL: "https://todolistapp-202a8.firebaseio.com",
+    projectId: "todolistapp-202a8",
+    storageBucket: "todolistapp-202a8.appspot.com",
+    messagingSenderId: "883800232561",
+    appId: "1:883800232561:web:8a847b88cde8a20ab2c1d0"
+  }
 };
